366ff19224769292b2a5ada0e67621ad9e9d5baf
Kouichi OHTA <kouichi.ohta@gmail.com>
2017-11-01 21:52:49 +0900

    DialogResHolder の定義方法を変更

--- app/src/main/java/jp/aknot/materialdesigndemo/presentation/DialogActivity.java
+++ app/src/main/java/jp/aknot/materialdesigndemo/presentation/DialogActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.SparseArray;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -28,55 +29,76 @@ public class DialogActivity extends AppCompatActivity implements AkDialogFragmen
 
     private static final String TAG = "@" + DialogActivity.class.getSimpleName();
 
-    private static final DialogResHolder[] DIALOG_RES_HOLDERS;
+    private static final int DLG_1_ALERT_ID = 1;
+    private static final int DLG_2_ALERT_ID = 2;
+    private static final int DLG_3_ALERT_ID = 3;
+    private static final int DLG_4_ALERT_ID = 4;
+    private static final int DLG_5_ALERT_ID = 5;
+    private static final int DLG_6_ALERT_ID = 6;
+    private static final int DLG_7_CONFIRMATION_ID = 7;
+    private static final int DLG_8_CONFIRMATION_ID = 8;
+    private static final int DLG_9_SIMPLE_ID = 9;
+    private static final int DLG_10_ITEM_LIST_ID = 10;
+
+    private static final SparseArray<DialogResHolder> DIALOG_RES_HOLDER_MAP = new SparseArray<>();
 
     static {
-        DIALOG_RES_HOLDERS = new DialogResHolder[]{
-                new AlertDialogResHolder(R.string.dialog_1_alert_title,
+        DIALOG_RES_HOLDER_MAP.put(DLG_1_ALERT_ID,
+                new AlertDialogResHolder(
+                        R.string.dialog_1_alert_title,
                         R.string.dialog_1_alert_msg,
                         R.string.dialog_1_alert_btn_positive,
                         R.string.dialog_1_alert_btn_negative,
-                        UNKNOWN_RES_ID),
+                        UNKNOWN_RES_ID));
+        DIALOG_RES_HOLDER_MAP.put(DLG_2_ALERT_ID,
                 new AlertDialogResHolder(R.style.MyThemeOverlay_Dialog_Alert,
                         R.string.dialog_2_alert_title,
                         R.string.dialog_2_alert_msg,
                         R.string.dialog_2_alert_btn_positive,
                         R.string.dialog_2_alert_btn_negative,
-                        UNKNOWN_RES_ID),
+                        UNKNOWN_RES_ID));
+        DIALOG_RES_HOLDER_MAP.put(DLG_3_ALERT_ID,
                 new AlertDialogResHolder(R.style.MyThemeOverlay_Dialog_Alert_Warn,
                         R.string.dialog_3_alert_title,
                         R.string.dialog_3_alert_msg,
                         R.string.dialog_3_alert_btn_positive,
                         R.string.dialog_3_alert_btn_negative,
-                        UNKNOWN_RES_ID),
-                new AlertDialogResHolder(R.string.dialog_4_alert_title,
+                        UNKNOWN_RES_ID));
+        DIALOG_RES_HOLDER_MAP.put(DLG_4_ALERT_ID,
+                new AlertDialogResHolder(
+                        R.string.dialog_4_alert_title,
                         R.string.dialog_4_alert_msg,
                         R.string.dialog_4_alert_btn_positive,
                         R.string.dialog_4_alert_btn_negative,
-                        UNKNOWN_RES_ID),
+                        UNKNOWN_RES_ID));
+        DIALOG_RES_HOLDER_MAP.put(DLG_5_ALERT_ID,
                 new AlertDialogResHolder(R.style.MyThemeOverlay_Dialog_Alert,
                         R.string.dialog_5_alert_title,
                         R.string.dialog_5_alert_msg,
                         R.string.dialog_5_alert_btn_positive,
                         R.string.dialog_5_alert_btn_negative,
-                        UNKNOWN_RES_ID),
+                        UNKNOWN_RES_ID));
+        DIALOG_RES_HOLDER_MAP.put(DLG_6_ALERT_ID,
                 new AlertDialogResHolder(R.style.MyThemeOverlay_Dialog_Alert_Warn,
                         R.string.dialog_6_alert_title,
                         R.string.dialog_6_alert_msg,
                         R.string.dialog_6_alert_btn_positive,
                         R.string.dialog_6_alert_btn_negative,
-                        UNKNOWN_RES_ID),
+                        UNKNOWN_RES_ID));
+        DIALOG_RES_HOLDER_MAP.put(DLG_7_CONFIRMATION_ID,
                 new ConfirmationDialogResHolder(R.string.dialog_7_confirmation_title,
-                        R.array.dialog_7_confirmation_items),
+                        R.array.dialog_7_confirmation_items));
+        DIALOG_RES_HOLDER_MAP.put(DLG_8_CONFIRMATION_ID,
                 new ConfirmationDialogResHolder(R.style.MyThemeOverlay_Dialog_Alert,
                         R.string.dialog_8_confirmation_title,
-                        R.array.dialog_8_confirmation_items),
+                        R.array.dialog_8_confirmation_items));
+        DIALOG_RES_HOLDER_MAP.put(DLG_9_SIMPLE_ID,
                 new SimpleDialogResHolder(R.string.dialog_9_simple_title,
-                        R.array.dialog_9_simple_items),
+                        R.array.dialog_9_simple_items));
+        DIALOG_RES_HOLDER_MAP.put(DLG_10_ITEM_LIST_ID,
                 new ItemListDialogResHolder(R.string.dialog_10_item_list_title,
                         R.array.dialog_10_item_list_item_drawables,
-                        R.array.dialog_10_item_list_items),
-        };
+                        R.array.dialog_10_item_list_items));
     }
 
     @Override
@@ -85,10 +107,12 @@ public class DialogActivity extends AppCompatActivity implements AkDialogFragmen
         setContentView(R.layout.activity_dialog);
 
         int index = 1;
-        List<SimpleListAdapter.Item> itemList = new ArrayList<>(DIALOG_RES_HOLDERS.length);
-        for (DialogResHolder holder : DIALOG_RES_HOLDERS) {
+        int size = DIALOG_RES_HOLDER_MAP.size();
+        List<SimpleListAdapter.Item> itemList = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            DialogResHolder holder = DIALOG_RES_HOLDER_MAP.valueAt(i);
             String prefix = index + ". (" + holder.getClass().getSimpleName().substring(0, 1) + ") ";
-            String title = getString(holder.titleResId);
+            String title = holder.titleResId != UNKNOWN_RES_ID ? getString(holder.titleResId) : null;
             if (TextUtils.isEmpty(title)) {
                 if (holder instanceof AlertDialogResHolder) {
                     title = getString(((AlertDialogResHolder) holder).msgResId);
@@ -104,7 +128,7 @@ public class DialogActivity extends AppCompatActivity implements AkDialogFragmen
         listView.setAdapter(adapter);
         listView.setOnItemClickListener((adapterView, view, position, id) -> {
             int dialogId = position + 1;
-            DialogResHolder dialogResHolder = DIALOG_RES_HOLDERS[dialogId];
+            DialogResHolder dialogResHolder = DIALOG_RES_HOLDER_MAP.valueAt(dialogId);
             AkDialogHelper.showDialog(this, dialogId, dialogResHolder);
         });
 
@@ -118,12 +142,12 @@ public class DialogActivity extends AppCompatActivity implements AkDialogFragmen
         Log.d(TAG, "onAkDialogClicked: dialogId=" + dialogId + ", resultCode=" + resultCode + ", params=" + params);
         String action = null;
         switch (dialogId) {
-            case 0: // Alert
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
+            case DLG_1_ALERT_ID:
+            case DLG_2_ALERT_ID:
+            case DLG_3_ALERT_ID:
+            case DLG_4_ALERT_ID:
+            case DLG_5_ALERT_ID:
+            case DLG_6_ALERT_ID:
                 switch (resultCode) {
                     case DialogInterface.BUTTON_POSITIVE:
                         action = "pressed Positive Button";
@@ -138,8 +162,8 @@ public class DialogActivity extends AppCompatActivity implements AkDialogFragmen
                         break;
                 }
                 break;
-            case 6: // Confirmation
-            case 7:
+            case DLG_7_CONFIRMATION_ID:
+            case DLG_8_CONFIRMATION_ID:
                 switch (resultCode) {
                     case DialogInterface.BUTTON_POSITIVE:
                         int checkedItemId = params.getInt(AkDialogFragment.PARAM_CHECKED_ITEM_ID);
@@ -156,8 +180,8 @@ public class DialogActivity extends AppCompatActivity implements AkDialogFragmen
                         break;
                 }
                 break;
-            case 8: // Simple
-            case 9: // ItemList
+            case DLG_9_SIMPLE_ID:
+            case DLG_10_ITEM_LIST_ID:
                 if (params != null) {
                     int checkedItemId = params.getInt(AkDialogFragment.PARAM_CHECKED_ITEM_ID);
                     String checkedItemValue = params.getString(AkDialogFragment.PARAM_CHECKED_ITEM_VALUE);
