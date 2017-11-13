1ac540a19db7aef89aa2f903beb443fc3ede49dc
Kouichi OHTA <kouichi.ohta@gmail.com>
2017-11-13 20:52:38 +0900

    move to inner class

--- app/src/main/java/jp/aknot/materialdesigndemo/presentation/view/AkDialogFragment.java
+++ app/src/main/java/jp/aknot/materialdesigndemo/presentation/view/AkDialogFragment.java
@@ -3,9 +3,13 @@ package jp.aknot.materialdesigndemo.presentation.view;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.annotation.ArrayRes;
 import android.support.annotation.DrawableRes;
+import android.support.annotation.IdRes;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
@@ -21,17 +25,20 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jp.aknot.materialdesigndemo.R;
-import jp.aknot.materialdesigndemo.presentation.adapter.DialogItemAdapter;
 
 public class AkDialogFragment extends DialogFragment {
 
@@ -641,6 +648,109 @@ public class AkDialogFragment extends DialogFragment {
                 }
                 super.onClick(dialog, which);
             }
+        }
+    }
+
+    private static class DialogItemAdapter extends BaseAdapter {
+
+        private final Context context;
+
+        @LayoutRes
+        private final int layoutResId;
+
+        @IdRes
+        private final int textViewResId;
+
+        private List<Item> iconItems = new ArrayList<>();
+
+        private static final int NO_ICON_RES_ID = 0;
+
+        public DialogItemAdapter(@NonNull Context context, @LayoutRes int layoutResId, @IdRes int textViewResId, @ArrayRes int itemsResId) {
+            this(context, layoutResId, textViewResId, NO_ICON_RES_ID, itemsResId);
+        }
+
+        public DialogItemAdapter(@NonNull Context context,
+                @LayoutRes int layoutResId, @IdRes int textViewResId, @ArrayRes int iconItemsResId, @ArrayRes int itemsResId) {
+            this.context = context;
+            this.layoutResId = layoutResId;
+            this.textViewResId = textViewResId;
+
+            Resources resources = context.getResources();
+            TypedArray drawableTypedArray = null;
+            if (iconItemsResId != NO_ICON_RES_ID) {
+                drawableTypedArray = resources.obtainTypedArray(iconItemsResId);
+            }
+            TypedArray stringTypedArray = resources.obtainTypedArray(itemsResId);
+            try {
+                int length = stringTypedArray.length();
+                for (int i = 0; i < length; i++) {
+                    String text = stringTypedArray.getString(i);
+                    if (TextUtils.isEmpty(text)) {
+                        continue;
+                    }
+                    @DrawableRes int drawableResId = (drawableTypedArray != null) ? drawableTypedArray.getResourceId(i, NO_ICON_RES_ID) : NO_ICON_RES_ID;
+                    iconItems.add(new Item(drawableResId, text));
+                }
+            } finally {
+                if (drawableTypedArray != null) {
+                    drawableTypedArray.recycle();
+                }
+                stringTypedArray.recycle();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return iconItems.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return iconItems.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+            ViewHolder viewHolder;
+            if (view == null) {
+                view = LayoutInflater.from(context).inflate(layoutResId, viewGroup, false);
+                viewHolder = new ViewHolder();
+                viewHolder.tvText = view.findViewById(textViewResId);
+                view.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) view.getTag();
+            }
+
+            Item item = (Item) getItem(position);
+            if (item.iconResId != NO_ICON_RES_ID) {
+                viewHolder.tvText.setCompoundDrawablesWithIntrinsicBounds(item.iconResId, 0, 0, 0);
+            }
+            viewHolder.tvText.setText(item.text);
+
+            return view;
+        }
+
+        private static class Item {
+
+            @DrawableRes
+            public final int iconResId;
+
+            @NonNull
+            public final String text;
+
+            public Item(@DrawableRes int iconResId, @NonNull String text) {
+                this.iconResId = iconResId;
+                this.text = text;
+            }
+        }
+
+        private static class ViewHolder {
+            TextView tvText;
         }
     }
 }
